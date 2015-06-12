6d48f1c73328ab0ed69e701428d3a7a7f5ae2db2
zukai <tsukai2012@163.com>
2015-06-12 12:48:36 +0800

    topbar init commit

---
+++ mytopbar/src/main/java/cn/beijing/zukai/mytopbar/Topbar.java
@@ -0,0 +1,26 @@
+package cn.beijing.zukai.mytopbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by zukai on 2015/06/12.
+ */
+public class Topbar extends RelativeLayout {
+    public Topbar(Context context) {
+        super(context);
+    }
+
+    public Topbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Topbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+   /* public Topbar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }*/
+}
