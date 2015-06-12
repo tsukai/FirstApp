ed3988fe4735a7115ae725a5ad1ee8c3d1e5b059
zukai <tsukai2012@163.com>
2015-06-12 11:21:28 +0800

    完善

--- newsimooc/src/main/java/cn/beijing/zukai/newsimooc/MainActivity.java
+++ newsimooc/src/main/java/cn/beijing/zukai/newsimooc/MainActivity.java
@@ -1,38 +1,114 @@
 package cn.beijing.zukai.newsimooc;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.beijing.zukai.newsimooc.bean.NewsBean;
+import cn.beijing.zukai.newimooc.adapter.NewsAdapter;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private ListView mListView;
+    private static String URL = "http://www.imooc.com/api/teacher?type=4&num=30";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.lv_main);
+        new NewsAsyncTask().execute(URL);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    /**
+     * 实现网络的异步访问
+     */
+    class NewsAsyncTask extends AsyncTask<String, Void, List<NewsBean>> {
+        @Override
+        protected List<NewsBean> doInBackground(String... params) {
+            return getJsonData(params[0]);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(List<NewsBean> newsBeans) {
+            super.onPostExecute(newsBeans);
+            NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, newsBeans);
+            mListView.setAdapter(newsAdapter);
+        }
+    }
+
+    /**
+     * 将url对应的json格式的数据转化为对象（NewsBean）
+     *
+     * @param param url
+     * @return
+     */
+    private List<NewsBean> getJsonData(String param) {
+        List<NewsBean> newsList = new ArrayList<NewsBean>();
+        try {
+            String jsonStr = readStream(new URL(param).openStream());
+            JSONObject jsonObject;
+            NewsBean newsBean;
+            jsonObject = new JSONObject(jsonStr);
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                newsBean = new NewsBean();
+                newsBean.setNewsIconUrl(obj.getString("picSmall"));
+                newsBean.setNewTitle(obj.getString("name"));
+                newsBean.setNewsContent(obj.getString("description"));
+                newsList.add(newsBean);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return newsList;
+    }
+
+    /**
+     * 通过InputStream解析玩野返回的数据
+     *
+     * @param is
+     * @return
+     */
+    private String readStream(InputStream is) {
+        InputStreamReader isr;
+        StringBuffer result = new StringBuffer("");
+        try {
+            String line = "";
+            isr = new InputStreamReader(is, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 }
